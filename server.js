92ed85273ad1db3d86909cf72c7c63153620723e
Ricardo Salazar <no-replay>
2021-09-01 21:03:40 -0500

    add server js file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.use(require("./routes/api"));
+app.use(require("./routes/html"));
+
+app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`)
+});
