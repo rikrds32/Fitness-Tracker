8a73b72933ef3ff0880c00676ec1cca299768926
Ricardo Salazar <no-replay>
2021-09-01 22:20:34 -0500

    add router get put and post to api js file

--- routes/api.js
+++ routes/api.js
@@ -16,3 +16,46 @@ router.get("/api/workouts", (req, res) => {
         res.status(400).json(err)
     })
 });
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then(workout => {
+        res.json(workout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { excercises: body } },
+        { new: true, runValidators: true }
+    )
+    .then(workout => {
+        res.json(workout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration"}
+            }
+        }
+    ])
+    .sort({ _id: -1  }).limit(7)
+    .then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+});
+
+module.exports = router;
