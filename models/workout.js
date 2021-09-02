f8369be213be31d672447cb3d9b5f01226c0c36a
Ricardo Salazar <no-replay>
2021-09-01 23:14:38 -0500

    corrected spelling on line 10

--- models/workout.js
+++ models/workout.js
@@ -7,7 +7,7 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    excercises: [
+    exercises: [
         {
             type: {
                 type: String,
